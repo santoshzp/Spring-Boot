1699ea5fb2c172d857c4581964c990f129bfd2f2
MacBook <santoshzp55@gmail.com>
2019-05-30 08:39:18 +0530

    change properties

--- src/main/java/com/javainterviewpoint/StudentController.java
+++ src/main/java/com/javainterviewpoint/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudentController
+public class  StudentController
 {
        @Autowired
     StudentRepository studentRepository;
