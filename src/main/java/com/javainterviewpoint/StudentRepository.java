6f8c3965d8d36796dfdc6ac263dfba5f3cdd217e
MacBook <santoshzp55@gmail.com>
2019-05-31 21:32:05 +0530

    gitignore

--- src/main/java/com/javainterviewpoint/StudentRepository.java
+++ src/main/java/com/javainterviewpoint/StudentRepository.java
@@ -2,9 +2,10 @@ package com.javainterviewpoint;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 
-@RepositoryRestResource
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long> 
 {
     
